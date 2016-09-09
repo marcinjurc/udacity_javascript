8e9b199f4ed00c1b26e8fe865c15284dd5381579
marcinjurc <jurczynski.marcin@gmail.com>
2016-09-09 07:08:42 +0200

    lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ //$("#main").append("Marcin");
+
+ //var awesomeThoughts = "I'm Marcin and I am AWESOME";
+ //var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+ //$("#main").append(funThoughts);
+
+ var formattedName = HTMLheaderName.replace("%data%", "Marcin Jurczyński");
+ var formatedRole = HTMLheaderRole.replace("%data%","Web Developer");
+
+ $("#header").prepend(formattedName);
+ $("#header").append(formatedRole);
